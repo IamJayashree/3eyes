92ce011042e3d9dfd79172cdef8ee31030a00e4e
manva001 <varun.mani@staples.com>
2021-10-21 20:37:38 -0400

    Testing

--- src/signup/signup.js
+++ src/signup/signup.js
@@ -25,8 +25,8 @@ export default function SignUp(props) {
   return (
     <>
       <img className="wave" src={WaveSVG} alt="3I" />
-        <div className="container px-4 py-5">
-          <div className="row flex-lg-row-reverse align-items-center g-5 py-5">
+      <div className="container px-4 py-5">
+        <div className="row flex-lg-row-reverse align-items-center g-5 py-5">
           <div className="col-10 col-sm-8 col-lg-6 d-none d-lg-block m-auto">
             <img src={SignUpSVG} className="d-block mx-lg-auto img-fluid" alt="3I" width="700" height="500" loading="lazy" />
           </div>
@@ -65,6 +65,7 @@ export default function SignUp(props) {
                 Sign Up
               </Button>
             </Form>
+
           </div>
         </div>
       </div>
