78bdfd6c0c8bdbba389792f65d798e2a4af69c35
manva001 <varun.mani@staples.com>
2021-10-24 12:48:15 -0400

    Adding condiiton

--- src/assistant/assistivepage.js
+++ src/assistant/assistivepage.js
@@ -1,10 +1,18 @@
+import React, { useState } from "react";
 
 export default function AssistivePage(props) {
 
+    const [activeState, setActiveState] = useState(false);
+
+    const [activeState1, setActiveState1] = useState(false);
+
     return (<>
 
-        Welcome to your perosnal assistive page
+        <h2>Assistive service made for hearing aids</h2>
 
 
+
+        <input type="button" onClick={() => { setActiveState(!activeState); }} value="sample"
+            className={activeState ? "btn btn-lg btn-primary rounded-pill py-3 w-100" : ""} />
     </>);
 }
