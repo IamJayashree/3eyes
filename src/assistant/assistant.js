34f6cd7dbc646b6d5785cbf6862053db0bdc6bc7
Girish <grsh84@gmail.com>
2021-10-24 19:22:26 +0530

    Fix the UI design of landing page

--- src/assistant/assistant.js
+++ src/assistant/assistant.js
@@ -50,7 +50,6 @@ export default function Assistant(props) {
                     }
                 });
             }
-
         }
         setValidated(true);
     };
@@ -58,106 +57,87 @@ export default function Assistant(props) {
     return (
         <>
             {userName ? <LogoutNavBar /> : <LoginNavBar />}
+            <Form onSubmit={handleSubmit} className="needs-validation" noValidate validated={validated}>
 
-            <div className="container px-4 py-4">
-                <Form onSubmit={handleSubmit} className="needs-validation" noValidate validated={validated}>
-                    <div className="card shadow">
-                        <div className="row">
-                            <div className="col-lg-8">
-                                <div className="card-body p-4 p-md-5">
-                                    <h3 class="fw-bold lh-1 mb-3">Request Assistant</h3>
-                                    <p class="mb-5">Please provide some more information about yourself and the kind of assistant you need.</p>
-                                    <div className="row g-3">
-                                        <div className="col-md-12">
-                                            <Form.Group size="lg" controlId="helpTitle">
-                                                <Form.Label className="form-label">What can we help you with?</Form.Label>
-                                                <Form.Control className="form-control"
-                                                    autoFocus
-                                                    type="text"
-                                                    name="helpTitle"
-                                                    placeholder="Example: Need a ride, Lost way"
-                                                    required />
-                                                <Form.Control.Feedback type="invalid" className="invalid-feedback">
-                                                    Valid help type is required.
-                                                </Form.Control.Feedback>
-                                            </Form.Group>
-                                        </div>
+                <h5 className="border-bottom pb-2 mb-2">Request Assistant</h5>
+                <p class="mb-3">Please provide some more information about yourself and the kind of assistant you need.</p>
 
-                                        <div className="col-md-12">
-                                            <label className="form-label">Assistant Category</label>
-                                            <br />
-                                            <select className="form-select" onChange={handleSelect}>
-                                                <option value="">Please select </option>
-                                                <option value="Emergency">Advisory Assistance</option>
-                                                <option value="Recreational">Mental Health</option>
-                                                <option value="Community">Personal Care</option>
-                                                <option value="Community">Educational</option>
-                                            </select>
-                                        </div>
-                                        <div className="col-md-6">
-                                            <Form.Group size="lg" controlId="date">
-                                                <Form.Label className="form-label">Date</Form.Label>
-                                                <Form.Control className="form-control"
-                                                    type="date"
-                                                    name="date"
-                                                    placeholder="MM/DD/YYY"
-                                                    required />
-                                                <Form.Control.Feedback type="invalid" className="invalid-feedback">
-                                                    Please input a valid Date.
-                                                </Form.Control.Feedback>
-                                            </Form.Group>
-                                        </div>
-                                        <div className="col-md-6">
-                                            <Form.Group size="lg" controlId="time">
-                                                <Form.Label className="form-label">Time</Form.Label>
-                                                <Form.Control className="form-control"
-                                                    type="time"
-                                                    placeholder="00:00 am - 00:00 am"
-                                                    name="time"
-                                                    required />
-                                                <Form.Control.Feedback type="invalid" className="invalid-feedback">
-                                                    Please input a valid Time.
-                                                </Form.Control.Feedback>
-                                            </Form.Group>
-                                        </div>
-                                        <div className="col-12">
-                                            <Form.Group size="lg" controlId="info">
-                                                <Form.Label className="form-label">More information to assist you better</Form.Label>
-                                                <Form.Control className="form-control"
-                                                    as="textarea"
-                                                    name="info"
-                                                    cols="30" rows="5"
-                                                    required />
-                                                <Form.Control.Feedback type="invalid" className="invalid-feedback">
-                                                    Please enter a valid info
-                                                </Form.Control.Feedback>
-                                            </Form.Group>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="col d-none d-md-block">&nbsp;</div>
-                        </div>
+                <Form.Group size="lg" controlId="helpTitle" className="mb-3">
+                    <Form.Label className="form-label">What can we help you with?</Form.Label>
+                    <Form.Control className="form-control"
+                        autoFocus
+                        type="text"
+                        name="helpTitle"
+                        placeholder="Example: Need a ride, Lost way"
+                        required />
+                    <Form.Control.Feedback type="invalid" className="invalid-feedback">
+                        Valid help type is required.
+                    </Form.Control.Feedback>
+                </Form.Group>
 
-                        <div className="card-footer py-4 px-5 d-grid gap-3 d-md-flex justify-content-md-start">
-                            <Button size="lg" type="submit" className="px-5 btn btn-primary btn-lg">
-                                Request Assistant
-                            </Button>
-                            {userName ? <>
-                                <a href="/LandingPage" className="px-5 btn btn-outline-primary btn-lg">
-                                    Cancel
-                                </a>
-                            </>
-                                :
-                                <>
-                                    <a href="/" className="px-5 btn btn-outline-primary btn-lg">
-                                        Cancel
-                                    </a>
-                                </>}
-                        </div>
-                    </div>
-                </Form>
-            </div>
+                <label className="form-label">Assistant Category</label>
+                <br />
+                <select onChange={handleSelect} className="form-select mb-3">
+                    <option value="">Please select </option>
+                    <option value="Emergency">Advisory Assistance</option>
+                    <option value="Recreational">Mental Health</option>
+                    <option value="Community">Personal Care</option>
+                    <option value="Community">Educational</option>
+                </select>
+
+                <Form.Group size="lg" controlId="date" className="mb-3">
+                    <Form.Label className="form-label">Date</Form.Label>
+                    <Form.Control className="form-control"
+                        type="date"
+                        name="date"
+                        placeholder="MM/DD/YYY"
+                        required />
+                    <Form.Control.Feedback type="invalid" className="invalid-feedback">
+                        Please input a valid Date.
+                    </Form.Control.Feedback>
+                </Form.Group>
+
+                <Form.Group size="lg" controlId="time" className="mb-3">
+                    <Form.Label className="form-label">Time</Form.Label>
+                    <Form.Control className="form-control"
+                        type="time"
+                        placeholder="00:00 am - 00:00 am"
+                        name="time"
+                        required />
+                    <Form.Control.Feedback type="invalid" className="invalid-feedback">
+                        Please input a valid Time.
+                    </Form.Control.Feedback>
+                </Form.Group>
+            
+                <Form.Group size="lg" controlId="info" className="mb-3">
+                    <Form.Label className="form-label">More information to assist you better</Form.Label>
+                    <Form.Control className="form-control"
+                        as="textarea"
+                        name="info"
+                        cols="30" rows="5"
+                        required />
+                    <Form.Control.Feedback type="invalid" className="invalid-feedback">
+                        Please enter a valid info
+                    </Form.Control.Feedback>
+                </Form.Group>
+                <Button type="submit" className="mb-3 w-100">
+                    Request Assistant
+                </Button>
+                {
+                    userName ?
+                    <>
+                        <a href="/LandingPage" className="btn btn-outline-primary w-100">
+                            Cancel
+                        </a>
+                    </>
+                    :
+                    <>
+                        <a href="/" className="btn btn-outline-primary w-100">
+                            Cancel
+                        </a>
+                    </>
+                }
+            </Form>
         </>
     );
 }
